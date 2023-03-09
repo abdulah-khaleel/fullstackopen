9858b4619be90f411109e96c982ac665e6c84a41
abdulah-khaleel <abdulah.khaleel@gmail.com>
2023-03-09 15:12:53 +0500

    [Add] exercise 2.5

--- part2/courseinfo/src/components/Course/Course.js
+++ part2/courseinfo/src/components/Course/Course.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "../Header/Header";
-import Content from "../Content/Content";
-import Sum from "../Sum/Sum";
+import Header from "./Header/Header";
+import Content from "./Content/Content";
+import Sum from "./Sum/Sum";
 
 const Course = ({ course }) => {
   return (
