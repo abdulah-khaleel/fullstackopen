cc59abafd90cbc042fdb8af696f081d17a387558
abdulah-khaleel <abdulah.khaleel@gmail.com>
2023-03-09 17:43:13 +0500

    [Add] exercise 2.6 phonebook

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    console.log(event.target.value);
+    const personObj = {
+      name: newName,
+    };
+    setPersons((oldArr) => persons.concat(personObj));
+    setNewName("");
+  };
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const numbersEl = persons.map((person) => (
+    <p key={Math.random()}>{person.name}</p>
+  ));
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input onChange={handleNameChange} value={newName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>{numbersEl}</div>
+    </div>
+  );
+};
+
+export default App;
